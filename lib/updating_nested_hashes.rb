454a2d9f1171f48e5b92f54d152fd0688975c2db
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-02 16:37:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -21,7 +21,7 @@ def update_status
    }
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  epic_tragedy[:montague][:hero]
+  epic_tragedy[:montague][:hero][:status] = "dead"
 
 
 
